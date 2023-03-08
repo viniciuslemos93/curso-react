36d09a6db7965ecd05e3d0db367696f881277d2f
Vinicius Lemos <vinycius1993@gmail.com>
2023-03-08 11:36:34 -0300

    feat: reaproveitando componentes

--- 2_FUNDAMENTOS/fundamentos/src/App.js
+++ 2_FUNDAMENTOS/fundamentos/src/App.js
@@ -5,6 +5,7 @@ import FirstComponent from "./components/FirstComponent"
 // styles | css
 import './App.css';
 import TemplateExpressions from "./components/TemplateExpressions";
+import MyComponent from "./components/MyComponent";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <h1>Fundamentos React</h1>
       <FirstComponent />
       <TemplateExpressions />
+      <MyComponent />
     </div>
   );
 }
--- 2_FUNDAMENTOS/fundamentos/src/components/FirstComponent.js
+++ 2_FUNDAMENTOS/fundamentos/src/components/FirstComponent.js
@@ -1,8 +1,11 @@
+import MyComponent from "./MyComponent";
+
 const FirstComponent = () => {
 
     return (
         <div>
-            <h1>Meu primeiro componente!</h1>            
+            <h1>Meu primeiro componente!</h1>
+            <MyComponent />
         </div>
     );
 
