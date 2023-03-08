1a358e6c09c4eea6fcbb36b15e03d48c3f35651b
Vinicius Lemos <vinycius1993@gmail.com>
2023-03-08 11:13:38 -0300

    feat: importando o componente

--- 2_FUNDAMENTOS/fundamentos/src/App.js
+++ 2_FUNDAMENTOS/fundamentos/src/App.js
@@ -1,10 +1,15 @@
-import logo from './logo.svg';
+// components
+import FirstComponent from "./components/FirstComponent"
+
+
+// styles | css
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Fundamentos React</h1>
+      <FirstComponent />
     </div>
   );
 }
