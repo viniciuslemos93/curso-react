eceb5c2ad635cb56b598990d656085a9fb767315
Vinicius Lemos <vinycius1993@gmail.com>
2023-03-09 13:51:37 -0300

    feat: introdução aos hooks

--- 3_AVANCANDO-NOS-FUNDAMENTOS/secao3-avancando-no-react/src/App.js
+++ 3_AVANCANDO-NOS-FUNDAMENTOS/secao3-avancando-no-react/src/App.js
@@ -1,10 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+
+import City from './assets/city.jpg'
+
+import ManageData from './components/ManageData';
 
 function App() {
   return (
     <div className="App">
-      <h1>Avançando em React</h1>
+      <h1>Avançando em React</h1>      
+      <div>
+        {/*Imagem em public */}
+        <img src="/img1.jpg" alt="Paisagem" />
+      </div>
+      <div>
+        {/*Imagem em src > assets a importação é dinâmica, com o nome da importação.
+        Isso é melhor pra manutenção, caso troca a pasta de local por exemplo, será alterado em um local apenas.*/}
+        <img src={City} alt="Cidade" />
+      </div>
+      <ManageData />
     </div>
   );
 }
---
+++ 3_AVANCANDO-NOS-FUNDAMENTOS/secao3-avancando-no-react/src/components/ManageData.js
@@ -0,0 +1,23 @@
+import {useState} from 'react'
+
+const ManageData = () => {
+    let someData = 10;
+    
+    {/*Sintaxe base do useState */}
+    const [number, setNumber] = useState(15)
+
+  return (
+    <div>
+        <div>
+            <p>Valor: {someData}</p>
+            <button onClick={() => someData = 15}>Mudar Variável</button>
+        </div>
+        <div>
+            <p>Valor: {number}</p>
+            <button onClick={() => setNumber (25)}>Mudar o state</button>
+        </div>
+    </div>
+  )
+}
+
+export default ManageData
