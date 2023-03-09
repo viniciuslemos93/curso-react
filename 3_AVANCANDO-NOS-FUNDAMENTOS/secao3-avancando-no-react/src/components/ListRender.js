9d8c40b5d2e64343efe2df244e385fea28e30cc7
Vinicius Lemos <vinycius1993@gmail.com>
2023-03-09 14:20:09 -0300

    feat: Iterando lista utilizando a chave key no map

--- 3_AVANCANDO-NOS-FUNDAMENTOS/secao3-avancando-no-react/src/components/ListRender.js
+++ 3_AVANCANDO-NOS-FUNDAMENTOS/secao3-avancando-no-react/src/components/ListRender.js
@@ -3,6 +3,12 @@ import {useState} from 'react'
 const ListRender = () => {
     const [list] = useState(["Vinicius", "Pedro", "Josias", "Maria"])
 
+    const [users] = useState([
+        {id: 1, name: "Vincius", age: 29},
+        {id: 7584, name: "João", age: 25},
+        {id: 1111, name: "Davi", age: 32},
+    ])
+
   return (
     <div>
         <ul>{/*Esta ArrowFunction do map, utilizamos o () ao invés de {}, por que o jsx é considerado um objeto,
@@ -10,8 +16,22 @@ const ListRender = () => {
             {list.map((item) => (
                 <li>{item}</li>
             ))}
-            {/*OBS.: Iterar listas sem a propriedade Key, gerará Warning no console!*/}
+            {/*OBS.: Iterar listas sem a propriedade Key, gerará Warning no console!
+            Portando, o React precisa de uma chave única em cada um dos ítens iterados. Isso ajuda e renderizar o componente.*/}
+
+            {/*Agora usando a Key no map! Assim não gera o Warning no console. É uma boa prática!*/}
+            {list.map((item, index) => (
+                <li key={index}>{item}</li>
+            ))}
         </ul>
+
+        <ul>
+            {/*Neste exemplo, como se o ID viesse do banco de dados, usamos para a key, o ID do usuário.*/}
+            {users.map((user) =>(
+                <li key={user.id}>{user.name} - {user.age}</li>
+            ))}
+        </ul>
+
     </div>
   )
 }
