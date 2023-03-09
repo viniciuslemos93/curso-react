e50d5c4c8aa98d8a265242f1a7c2834a218216dd
Vinicius Lemos <vinycius1993@gmail.com>
2023-03-09 09:33:26 -0300

    feat: colocando jsx fora do return do componente

--- 2_FUNDAMENTOS/fundamentos/src/components/Events.js
+++ 2_FUNDAMENTOS/fundamentos/src/components/Events.js
@@ -3,6 +3,16 @@ const Events = () => {
     const handleMyEvent = (e) => {
         {/*O parâmetro "e" são os dados que o evento contém  */}
         console.log(e)
+
+        console.log("Ativou o evento!")
+    };
+
+    const renderSomething = (x) => {
+        if (x) {
+            return <h1>Renderizando isso.</h1>
+        }else {
+            return <h1>Também posso renderizar isso.</h1>
+        }
     }
 
     return (
@@ -11,6 +21,23 @@ const Events = () => {
                 {/*Ao chamar o evento, se colocar parênteses () ele chama o evento ao carregar a página*/}
                 <button onClick={handleMyEvent}>Clique aqui!</button>
             </div>
+            <div>
+                <button onClick={() => console.log("Clicou na Arrowfunction direto no evento!")}>
+                    Clique aqui também!                
+                </button>
+                <button                    
+                    onClick={() => {
+                        {/*Desta maneira por ser uma função maior, não é indicado fazer assim com a função direto no evento! */}
+                        if(true) {
+                            console.log("Aqui não é indicado fazer!");
+                        }                    
+                    }}
+                >
+                    Clique aqui
+                </button>
+            </div>
+            {renderSomething(true)}
+            {renderSomething(false)}
         </div>
     );
 };
