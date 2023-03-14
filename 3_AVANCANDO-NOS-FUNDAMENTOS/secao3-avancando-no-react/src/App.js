d86f76d430c5089ec8313b4b667e69a276486bdb
Vinicius Lemos <vinycius1993@gmail.com>
2023-03-14 11:52:13 -0300

    feat: acrescentando IF ternário na renderização

--- 3_AVANCANDO-NOS-FUNDAMENTOS/secao3-avancando-no-react/src/App.js
+++ 3_AVANCANDO-NOS-FUNDAMENTOS/secao3-avancando-no-react/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 
 import City from './assets/city.jpg'
+import ConditionalRender from './components/ConditionalRender';
 import ListRender from './components/ListRender';
 
 import ManageData from './components/ManageData';
@@ -21,6 +22,8 @@ function App() {
       <ManageData />
 
       <ListRender />
+
+      <ConditionalRender />
     </div>
   );
 }
