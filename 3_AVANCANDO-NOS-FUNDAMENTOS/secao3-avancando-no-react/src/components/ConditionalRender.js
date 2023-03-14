19df21413eb37edd2ef3fb7359e1101bf6e6ac32
Vinicius Lemos <vinycius1993@gmail.com>
2023-03-14 13:42:29 -0300

    feat: utilizando props

--- 3_AVANCANDO-NOS-FUNDAMENTOS/secao3-avancando-no-react/src/components/ConditionalRender.js
+++ 3_AVANCANDO-NOS-FUNDAMENTOS/secao3-avancando-no-react/src/components/ConditionalRender.js
@@ -3,11 +3,25 @@ import { useState } from "react"
 const ConditionalRender = () => {
   const [x] = useState(false)
 
+  const [name, setName] = useState("Vinicius")
+
   return (
   <div>
     {/*Condicional simples para checar se o valor é verdadeiro ou falso, e imprime ou não. */}
     <h1>Isso será exibido?</h1>
     {x && <p>Se x for true, sim!</p>}
+    {/*Condicional | IF ternário*/}
+    {name === "João" ? (
+      <div>
+        <p>O nome é João!</p>
+      </div>
+    ) : (
+      <div>
+        <p>Nome não encontrado!</p>
+      </div>
+    )}
+    {/*Botão setando o nome para João*/}
+    <button onClick={() => setName("João") }>Clica aqui</button>
   </div>
   )
 }
