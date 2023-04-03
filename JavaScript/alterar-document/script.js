2d1c0d032b9aae4ddc7bc243c9ec1795933823a4
Vinicius Lemos <vinycius1993@gmail.com>
2023-04-03 10:50:11 -0300

    feat: alterando Document com JS

---
+++ JavaScript/alterar-document/script.js
@@ -0,0 +1,10 @@
+//Propriedades document
+console.log(document.body)
+
+console.log(document.head)
+
+console.log(document.links[0])
+
+console.log(document.URL)
+
+document.title = "Manipulando Document com Javascript"
