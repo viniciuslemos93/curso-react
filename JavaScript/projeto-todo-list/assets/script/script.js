67994821181c2d0b87096bdd1ffdce9c98fc0241
Vinicius Lemos <vinycius1993@gmail.com>
2023-04-05 10:06:11 -0300

    feat: criada a função para adicionar tasks

--- JavaScript/projeto-todo-list/assets/script/script.js
+++ JavaScript/projeto-todo-list/assets/script/script.js
@@ -9,7 +9,19 @@ function addTask() {
 
         const newTask = template.cloneNode(true)
 
-        console.log (newTask)
+        // adiciona título
+        newTask.querySelector(".task-title").textContent = taskTitle
+
+        // remover as classes desnecessárias
+        newTask.classList.remove("template")
+        newTask.classList.remove("hide")
+
+        // adiciona tarefa na lista
+        const list = document.querySelector("#task-list")
+        list.appendChild(newTask)
+
+        // limpar texto
+        document.querySelector("#task-title").value = ""
     }
 }
 
