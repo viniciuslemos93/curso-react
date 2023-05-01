de4aa89c1bdc10bb8becf5e5fe2f107ba18532b2
Vinicius Lemos <vinycius1993@gmail.com>
2023-05-01 19:03:38 -0300

    style: inline css

--- 4_CSS_REACT/css/src/App.js
+++ 4_CSS_REACT/css/src/App.js
@@ -4,6 +4,10 @@ function App() {
   return (
     <div className="App">
       <h1>React com CSS</h1>
+      {/* Inline CSS */}
+      <p style={{ color: "blue", padding: "25px", borderTop: "2px solid red" }}>
+        Este elemento foi estilizado de forma Inline
+      </p>
     </div>
   );
 }
